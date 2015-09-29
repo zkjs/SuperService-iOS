9c3a003b9a5a4a7e9e3c3ba6250335e410d2c860
hanton <hanton.yang@gmail.com>
2015-09-29 17:28:05 +0800

    Add LTBouncyTextField

---
+++ SuperService/SuperService/RoundedRectButton.swift
@@ -0,0 +1,21 @@
+//
+//  RoundedRectButton.swift
+//  SuperService
+//
+//  Created by Hanton on 9/29/15.
+//  Copyright © 2015 ZKJS. All rights reserved.
+//
+
+import UIKit
+
+class RoundedRectButton: UIButton {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}

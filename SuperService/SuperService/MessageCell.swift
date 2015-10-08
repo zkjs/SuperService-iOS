1654e8cfc972aec0f2ae10ea1e708a6c2f1b3e4d
hanton <hanton.yang@gmail.com>
2015-10-08 19:51:33 +0800

    Pod: add XLPagerTabStrip

---
+++ SuperService/SuperService/MessageCell.swift
@@ -0,0 +1,24 @@
+//
+//  MessageCell.swift
+//  SuperService
+//
+//  Created by Hanton on 10/8/15.
+//  Copyright © 2015 ZKJS. All rights reserved.
+//
+
+import UIKit
+
+class MessageCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ SuperService/SuperService/OrderCell.swift
@@ -0,0 +1,24 @@
+//
+//  OrderCell.swift
+//  SuperService
+//
+//  Created by Hanton on 10/8/15.
+//  Copyright © 2015 ZKJS. All rights reserved.
+//
+
+import UIKit
+
+class OrderCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}

162441771e550ba16dcd38a6f6d5ef48eb954724
hanton <hanton.yang@gmail.com>
2015-10-14 16:12:46 +0800

    HTTP接口类 ZKJSHTTPSessionManager

--- SuperService/SuperService/ZKJSHTTPSessionManager.h
+++ SuperService/SuperService/ZKJSHTTPSessionManager.h
@@ -6,8 +6,14 @@
 //  Copyright © 2015 ZKJS. All rights reserved.
 //
 
-#import "AFHTTPSessionManager.h"
+#import "AFNetworking.h"
 
 @interface ZKJSHTTPSessionManager : AFHTTPSessionManager
+
+// 单例
++ (instancetype)sharedInstance;
+
+// 管理员登陆
+- (void)adminLoginWithPhone:(NSString *)phone password:(NSString *)password success:(void (^)(NSURLSessionDataTask *task, id responseObject))success failure:(void (^)(NSURLSessionDataTask *task, NSError *error))failure;
 
 @end
--- SuperService/SuperService/ZKJSHTTPSessionManager.m
+++ SuperService/SuperService/ZKJSHTTPSessionManager.m
@@ -7,7 +7,43 @@
 //
 
 #import "ZKJSHTTPSessionManager.h"
+#import "NSString+ZKJS.h"
 
 @implementation ZKJSHTTPSessionManager
+
+#pragma mark - Initialization
+
++ (instancetype)sharedInstance {
+  static dispatch_once_t once;
+  static id sharedInstance;
+  dispatch_once(&once, ^{
+    sharedInstance = [[self alloc] init];
+  });
+  return sharedInstance;
+}
+
+- (id)init {
+  self = [super initWithBaseURL:[[NSURL alloc] initWithString:@"http://120.25.241.196/"]];
+  if (self) {
+    self.requestSerializer = [[AFHTTPRequestSerializer alloc] init];
+    self.responseSerializer = [[AFJSONResponseSerializer alloc] init];
+  }
+  return self;
+}
+
+#pragma mark - 管理员登陆
+
+- (void)adminLoginWithPhone:(NSString *)phone password:(NSString *)password success:(void (^)(NSURLSessionDataTask *task, id responseObject))success failure:(void (^)(NSURLSessionDataTask *task, NSError *error))failure {
+  [self POST:@"semp/semplogin" parameters:nil constructingBodyWithBlock:^(id<AFMultipartFormData> formData) {
+    [formData appendPartWithFormData:[phone dataUsingEncoding:NSUTF8StringEncoding] name:@"phone"];
+    [formData appendPartWithFormData:[[password MD5String] dataUsingEncoding:NSUTF8StringEncoding] name:@"password"];
+  } success:^(NSURLSessionDataTask *task, id responseObject) {
+    NSLog(@"%@", [responseObject description]);
+    success(task, responseObject);
+  } failure:^(NSURLSessionDataTask *task, NSError *error) {
+    NSLog(@"%@", error.description);
+    failure(task, error);
+  }];
+}
 
 @end

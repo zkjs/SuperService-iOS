69c914956111894ec5689d2bb937a6af38788fd9
hanton <hanton.yang@gmail.com>
2015-10-16 09:49:47 +0800

    Pod: DateTools

--- SuperService/Pods/Target Support Files/Pods-SuperService/Pods-SuperService-resources.sh
+++ SuperService/Pods/Target Support Files/Pods-SuperService/Pods-SuperService-resources.sh
@@ -58,9 +58,11 @@ install_resource()
   esac
 }
 if [[ "$CONFIGURATION" == "Debug" ]]; then
+  install_resource "DateTools/DateTools/DateTools.bundle"
   install_resource "${BUILT_PRODUCTS_DIR}/WPMediaPicker.bundle"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
+  install_resource "DateTools/DateTools/DateTools.bundle"
   install_resource "${BUILT_PRODUCTS_DIR}/WPMediaPicker.bundle"
 fi
 

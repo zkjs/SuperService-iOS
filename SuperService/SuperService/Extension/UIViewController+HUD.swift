654bfd007d78e5b892454ddc9624edc4e3d5acc2
hanton <hanton.yang@gmail.com>
2015-11-12 10:25:09 +0800

    Extension: 打印VC类型当viewWillAppear

--- SuperService/SuperService/Extension/UIViewController+HUD.swift
+++ SuperService/SuperService/Extension/UIViewController+HUD.swift
@@ -56,3 +56,45 @@ extension UIViewController {
   }
   
 }
+
+extension UIViewController {
+  
+  public override class func initialize() {
+    struct Static {
+      static var token: dispatch_once_t = 0
+    }
+    
+    // make sure this isn't a subclass
+    if self !== UIViewController.self {
+      return
+    }
+    
+    dispatch_once(&Static.token) {
+      let originalSelector = Selector("viewWillAppear:")
+      let swizzledSelector = Selector("ZKJS_viewWillAppear:")
+      
+      let originalMethod = class_getInstanceMethod(self, originalSelector)
+      let swizzledMethod = class_getInstanceMethod(self, swizzledSelector)
+      
+      let didAddMethod = class_addMethod(self, originalSelector, method_getImplementation(swizzledMethod), method_getTypeEncoding(swizzledMethod))
+      
+      if didAddMethod {
+        class_replaceMethod(self, swizzledSelector, method_getImplementation(originalMethod), method_getTypeEncoding(originalMethod))
+      } else {
+        method_exchangeImplementations(originalMethod, swizzledMethod);
+      }
+    }
+  }
+  
+  // MARK: - Method Swizzling
+  
+  func ZKJS_viewWillAppear(animated: Bool) {
+    self.ZKJS_viewWillAppear(animated)
+    print(NSStringFromClass(self.dynamicType))
+//    if let name = self.descriptiveName {
+//      print("viewWillAppear: \(name)")
+//    } else {
+//      print("viewWillAppear: \(self)")
+//    }
+  }
+}

7e72f46ddfe91f6d4020506262766d4b3bf0b381
hanton <hanton.yang@gmail.com>
2015-12-10 17:35:03 +0800

    聊天支持分页取聊天记录

--- SuperService/SuperService/MessageCenter/EaseMob/EaseUI/EMUIKit/ViewController/EaseRefreshTableViewController.m
+++ SuperService/SuperService/MessageCenter/EaseMob/EaseUI/EMUIKit/ViewController/EaseRefreshTableViewController.m
@@ -22,151 +22,153 @@
 
 - (instancetype)initWithStyle:(UITableViewStyle)style
 {
-    self = [super init];
-    if (self) {
-        _style = style;
-    }
-    
-    return self;
+  self = [super init];
+  if (self) {
+    _style = style;
+  }
+  
+  return self;
 }
 
 - (void)viewDidLoad {
-    [super viewDidLoad];
-    
-    // Uncomment the following line to preserve selection between presentations.
-    if ([[[UIDevice currentDevice] systemVersion] floatValue] >= 7.0) {
-        self.edgesForExtendedLayout =  UIRectEdgeNone;
-    }
-    
-    _tableView = [[UITableView alloc] initWithFrame:CGRectMake(0, 0, self.view.frame.size.width, self.view.frame.size.height) style:self.style];
-    _tableView.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
-    _tableView.delegate = self;
-    _tableView.dataSource = self;
-    _tableView.tableFooterView = self.defaultFooterView;
-    [self.view addSubview:_tableView];
-    
-    _page = 0;
-    _showRefreshHeader = NO;
-    _showRefreshFooter = NO;
-    _showTableBlankView = NO;
+  [super viewDidLoad];
+  
+  // Uncomment the following line to preserve selection between presentations.
+  if ([[[UIDevice currentDevice] systemVersion] floatValue] >= 7.0) {
+    self.edgesForExtendedLayout =  UIRectEdgeNone;
+  }
+  
+  _tableView = [[UITableView alloc] initWithFrame:CGRectMake(0, 0, self.view.frame.size.width, self.view.frame.size.height) style:self.style];
+  _tableView.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
+  _tableView.delegate = self;
+  _tableView.dataSource = self;
+  _tableView.tableFooterView = self.defaultFooterView;
+  [self.view addSubview:_tableView];
+  
+  _page = 0;
+  _showRefreshHeader = NO;
+  _showRefreshFooter = NO;
+  _showTableBlankView = NO;
 }
 
 - (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+  [super didReceiveMemoryWarning];
+  // Dispose of any resources that can be recreated.
 }
 
 #pragma mark - setter
 
 - (void)setShowRefreshHeader:(BOOL)showRefreshHeader
 {
-    if (_showRefreshHeader != showRefreshHeader) {
-        _showRefreshHeader = showRefreshHeader;
-        if (_showRefreshHeader) {
-            __weak EaseRefreshTableViewController *weakSelf = self;
-          [MJRefreshHeader headerWithRefreshingBlock:^{
-                [weakSelf tableViewDidTriggerHeaderRefresh];
-                [weakSelf.tableView.mj_header beginRefreshing];
-            }];
-//            header.updatedTimeHidden = YES;
-        }
-        else{
-//            [self.tableView removeHeader];
-        }
+  if (_showRefreshHeader != showRefreshHeader) {
+    _showRefreshHeader = showRefreshHeader;
+    if (_showRefreshHeader) {
+      __weak EaseRefreshTableViewController *weakSelf = self;
+      MJRefreshStateHeader *header = [MJRefreshStateHeader headerWithRefreshingBlock:^{
+        [weakSelf tableViewDidTriggerHeaderRefresh];
+        [weakSelf.tableView.mj_header beginRefreshing];
+      }];
+      header.lastUpdatedTimeLabel.hidden = YES;
+      self.tableView.mj_header = header;
     }
+    else{
+      self.tableView.mj_header = nil;
+    }
+  }
 }
 
 - (void)setShowRefreshFooter:(BOOL)showRefreshFooter
 {
-    if (_showRefreshFooter != showRefreshFooter) {
-        _showRefreshFooter = showRefreshFooter;
-        if (_showRefreshFooter) {
-            __weak EaseRefreshTableViewController *weakSelf = self;
-            [MJRefreshAutoNormalFooter footerWithRefreshingBlock:^{
-                [weakSelf tableViewDidTriggerFooterRefresh];
-                [weakSelf.tableView.mj_footer beginRefreshing];
-            }];
-        }
-        else{
-//            [self.tableView removeFooter];
-        }
+  if (_showRefreshFooter != showRefreshFooter) {
+    _showRefreshFooter = showRefreshFooter;
+    if (_showRefreshFooter) {
+      __weak EaseRefreshTableViewController *weakSelf = self;
+      MJRefreshAutoNormalFooter *footer = [MJRefreshAutoNormalFooter footerWithRefreshingBlock:^{
+        [weakSelf tableViewDidTriggerFooterRefresh];
+        [weakSelf.tableView.mj_footer beginRefreshing];
+      }];
+      self.tableView.mj_footer = footer;
     }
+    else{
+      self.tableView.mj_footer = nil;
+    }
+  }
 }
 
 - (void)setShowTableBlankView:(BOOL)showTableBlankView
 {
-    if (_showTableBlankView != showTableBlankView) {
-        _showTableBlankView = showTableBlankView;
-    }
+  if (_showTableBlankView != showTableBlankView) {
+    _showTableBlankView = showTableBlankView;
+  }
 }
 
 #pragma mark - getter
 
 - (NSMutableArray *)dataArray
 {
-    if (_dataArray == nil) {
-        _dataArray = [NSMutableArray array];
-    }
-    
-    return _dataArray;
+  if (_dataArray == nil) {
+    _dataArray = [NSMutableArray array];
+  }
+  
+  return _dataArray;
 }
 
 - (NSMutableDictionary *)dataDictionary
 {
-    if (_dataDictionary == nil) {
-        _dataDictionary = [NSMutableDictionary dictionary];
-    }
-    
-    return _dataDictionary;
+  if (_dataDictionary == nil) {
+    _dataDictionary = [NSMutableDictionary dictionary];
+  }
+  
+  return _dataDictionary;
 }
 
 - (UIView *)defaultFooterView
 {
-    if (_defaultFooterView == nil) {
-        _defaultFooterView = [[UIView alloc] init];
-    }
-    
-    return _defaultFooterView;
+  if (_defaultFooterView == nil) {
+    _defaultFooterView = [[UIView alloc] init];
+  }
+  
+  return _defaultFooterView;
 }
 
 #pragma mark - Table view data source
 
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
 {
-    // Return the number of sections.
-    return 1;
+  // Return the number of sections.
+  return 1;
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
-    // Return the number of rows in the section.
-    return 0;
+  // Return the number of rows in the section.
+  return 0;
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
-    static NSString *CellIdentifier = @"UITableViewCell";
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
-    
-    // Configure the cell...
-    
-    return cell;
+  static NSString *CellIdentifier = @"UITableViewCell";
+  UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
+  
+  // Configure the cell...
+  
+  return cell;
 }
 
 #pragma mark - Table view delegate
 
 - (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
 {
-    return KCELLDEFAULTHEIGHT;
+  return KCELLDEFAULTHEIGHT;
 }
 
 #pragma mark - public refresh
 
 - (void)autoTriggerHeaderRefresh
 {
-    if (self.showRefreshHeader) {
-        [self tableViewDidTriggerHeaderRefresh];
-    }
+  if (self.showRefreshHeader) {
+    [self tableViewDidTriggerHeaderRefresh];
+  }
 }
 
 /**
@@ -174,7 +176,7 @@
  */
 - (void)tableViewDidTriggerHeaderRefresh
 {
-    
+  
 }
 
 /**
@@ -182,24 +184,24 @@
  */
 - (void)tableViewDidTriggerFooterRefresh
 {
-    
+  
 }
 
 - (void)tableViewDidFinishTriggerHeader:(BOOL)isHeader reload:(BOOL)reload
 {
-    __weak EaseRefreshTableViewController *weakSelf = self;
-    dispatch_async(dispatch_get_main_queue(), ^{
-        if (reload) {
-            [weakSelf.tableView reloadData];
-        }
-        
-        if (isHeader) {
-            [weakSelf.tableView.mj_header endRefreshing];
-        }
-        else{
-            [weakSelf.tableView.mj_footer endRefreshing];
-        }
-    });
+  __weak EaseRefreshTableViewController *weakSelf = self;
+  dispatch_async(dispatch_get_main_queue(), ^{
+    if (reload) {
+      [weakSelf.tableView reloadData];
+    }
+    
+    if (isHeader) {
+      [weakSelf.tableView.mj_header endRefreshing];
+    }
+    else{
+      [weakSelf.tableView.mj_footer endRefreshing];
+    }
+  });
 }
 
 @end

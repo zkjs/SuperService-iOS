4d0af056122a25d753c6b5947cf24fc41537fdd8
hanton <hanton.yang@gmail.com>
2015-12-30 11:41:58 +0800

    Add Watch Support

---
+++ SuperService/Watch Extension/InterfaceController.swift
@@ -0,0 +1,35 @@
+//
+//  InterfaceController.swift
+//  Watch Extension
+//
+//  Created by Hanton on 12/29/15.
+//  Copyright © 2015 ZKJS. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+  
+  override func awakeWithContext(context: AnyObject?) {
+    super.awakeWithContext(context)
+    
+    // Configure interface objects here.
+  }
+  
+  override func willActivate() {
+    // This method is called when watch view controller is about to be visible to user
+    super.willActivate()
+  }
+  
+  override func didDeactivate() {
+    // This method is called when watch view controller is no longer visible
+    super.didDeactivate()
+  }
+  
+  @IBAction func openParentApp() {
+    print("openParentApp")
+  }
+  
+}

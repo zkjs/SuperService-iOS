78a6dee177827608a7a0715b579c370c63db7f7a
hanton <hanton.yang@gmail.com>
2016-01-15 16:14:20 +0800

    Debug版本不打开短信验证码

--- SuperService/SuperService/Networking/ZKJSHTTPSMSSessionManager.m
+++ SuperService/SuperService/Networking/ZKJSHTTPSMSSessionManager.m
@@ -72,12 +72,16 @@ static NSString* const templateID = @"50158";
 
 // 检查短信验证码
 - (void)verifySmsCode:(NSString *)code mobilePhoneNumber:(NSString *)phone callback:(void (^)(BOOL succeeded, NSError *error))callback {
+#if DEBUG
+  callback(YES, nil);
+#else
   NSString *verifycode = self.phoneSMS[phone];
   if ([code isEqualToString:verifycode]) {
     callback(YES, nil);
   } else {
     callback(NO, nil);
   }
+#endif
 }
 
 @end

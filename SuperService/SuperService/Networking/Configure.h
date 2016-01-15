306599a5ca0c145bbc32a9f3e008bcc8f8c010d7
hanton <hanton.yang@gmail.com>
2016-01-15 15:53:22 +0800

    环信3个环境

--- SuperService/SuperService/Networking/Configure.h
+++ SuperService/SuperService/Networking/Configure.h
@@ -13,15 +13,19 @@
 #define kImageURL @"http://svip02.oss-cn-shenzhen.aliyuncs.com"
 
 // 测试
-//#define kBaseURL @"http://tst.zkjinshi.com/"  // 测试
-//#define kJavaBaseURL @"http://test.zkjinshi.com/japi/"  // 测试
+//#define kBaseURL @"http://tst.zkjinshi.com/"  // PHP服务器
+//#define kJavaBaseURL @"http://test.zkjinshi.com/japi/"  // Java服务器
+#define kEaseMobAppKey @"zkjs#svip"  // 环信
+
 
 // 预上线
-#define kBaseURL @"http://rap.zkjinshi.com/" // 预上线
-#define kJavaBaseURL @"http://p.zkjinshi.com/japi/"  // 预上线
+#define kBaseURL @"http://rap.zkjinshi.com/" // PHP服务器
+#define kJavaBaseURL @"http://p.zkjinshi.com/japi/"  // Java服务器
+//#define kEaseMobAppKey @"zkjs#sid"  // 环信
 
 // 正式
-//#define kBaseURL @"http://api.zkjinshi.com/" // 正式
-//#define kJavaBaseURL @"http://mmm.zkjinshi.com/"  // 正式
+//#define kBaseURL @"http://api.zkjinshi.com/" // PHP服务器
+//#define kJavaBaseURL @"http://mmm.zkjinshi.com/"  // Java服务器
+//#define kEaseMobAppKey @"zkjs#prosvip"  // 环信
 
 #endif /* Configure_h */

433dd90f016b82e68a927e19064d94a95fa621b4
hanton <hanton.yang@gmail.com>
2016-01-30 15:27:48 +0800

    App在后台收到消息时发本地推送

--- SuperService/SuperService/MainTBC.swift
+++ SuperService/SuperService/MainTBC.swift
@@ -304,6 +304,13 @@ extension MainTBC: EMCallManagerDelegate {
     EaseMob.sharedInstance().callManager.addDelegate(self, delegateQueue: nil)
   }
   
+  func didReceiveMessage(message: EMMessage!) {
+    let notification = UILocalNotification()
+    notification.alertBody = "您有一条新消息"
+    notification.soundName = UILocalNotificationDefaultSoundName
+    UIApplication.sharedApplication().presentLocalNotificationNow(notification)
+  }
+  
   // 未读消息数量变化回调
   
   func didUnreadMessagesCountChanged() {

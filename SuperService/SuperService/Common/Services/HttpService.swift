cc171ca70a5e95fa341e577d38595db47ecf976c
QinYejun <yejun@zkjinshi.com>
2016-04-21 17:49:24 +0800

    refresh token synchronous

--- SuperService/SuperService/Common/Services/HttpService.swift
+++ SuperService/SuperService/Common/Services/HttpService.swift
@@ -110,11 +110,12 @@ class HttpService {
       }
       if statusCode == 401 {//token过期
         // 由于异步请求，其他请求在token刷新后立即到达server会被判定失效，导致用户被登出
-        if NSDate().timeIntervalSince1970 > self.refreshTokenTime + 30 {
+        // 2016-04-21: 刷新token改为非异步方式 
+        //if NSDate().timeIntervalSince1970 > self.refreshTokenTime + 30 {
           print("invalid token:\(req)")
           TokenPayload.sharedInstance.clearCacheTokenPayload()
           NSNotificationCenter.defaultCenter().postNotificationName(KNOTIFICATION_LOGOUTCHANGE, object: nil)
-        }
+        //}
         
         let e = NSError(domain: NSBundle.mainBundle().bundleIdentifier ?? "com.zkjinshi.svip",
           code: 401,
